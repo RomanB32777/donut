82b5119cde21ac3f0dcb2fb25495472b359c0967
Roman Bogdanovskiy <r.bogdanovskiy@onpoint.ru>
2022-08-09 02:49:24 +0300

    node env react

--- client/src/axiosClient.ts
+++ client/src/axiosClient.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-export const baseURL = `https://${window.location.hostname}` // :${process.env.REACT_APP_BACKEND_PORT || 5000}
+export const baseURL = `https://${window.location.hostname}` + (
+    process.env.REACT_APP_NODE_ENV !== 'production' && `:${process.env.REACT_APP_BACKEND_PORT || 5000}`
+) 
 
 const axiosClient = axios.create({
-    baseURL: baseURL + '/'  // http://localhost:8080/
-    
+    baseURL: baseURL + '/'
 });
 
 export default axiosClient
