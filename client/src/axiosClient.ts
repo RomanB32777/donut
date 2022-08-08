9bd640b3bd58d789918eebb57b8af621643d5d59
Eugene Zarechkin <sovsemsk@gmail.com>
2022-08-09 02:15:04 +0300

    fix baseurl

--- client/src/axiosClient.ts
+++ client/src/axiosClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = `http://${window.location.hostname}:${process.env.REACT_APP_BACKEND_PORT || 5000}` // :${process.env.REACT_APP_BACKEND_PORT || 5000}
+export const baseURL = `https://${window.location.hostname}` // :${process.env.REACT_APP_BACKEND_PORT || 5000}
 
 const axiosClient = axios.create({
     baseURL: baseURL + '/'  // http://localhost:8080/
