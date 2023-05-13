841c3a6c1e6605c866531985a63c00c880d37df4
Roman Bogdanovskiy <r.bogdanovskiy@onpoint.ru>
2023-05-13 20:52:21 +0300

    fix(twitch migration): fix twitch migration

--- backend/migrations/1683992608220-twitch.ts
+++ backend/migrations/1683992608220-twitch.ts
@@ -1,22 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class twitch1683992608220 implements MigrationInterface {
-    name = 'twitch1683992608220'
+	name = 'twitch1683992608220'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "stats" RENAME COLUMN "text_alignment" TO "text_alignment2"`);
-        await queryRunner.query(`ALTER TYPE "public"."stats_text_alignment_enum" RENAME TO "stats_text_alignment2_enum"`);
-        await queryRunner.query(`ALTER TABLE "creators" RENAME COLUMN "test_color" TO "twitch_banner"`);
-        await queryRunner.query(`ALTER TABLE "creators" ALTER COLUMN "twitch_banner" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "creators" ALTER COLUMN "twitch_banner" SET DEFAULT ''`);
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "creators" ADD "twitch_banner" character varying DEFAULT ''`,
+		)
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "creators" ALTER COLUMN "twitch_banner" SET DEFAULT '#E94560'`);
-        await queryRunner.query(`ALTER TABLE "creators" ALTER COLUMN "twitch_banner" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "creators" RENAME COLUMN "twitch_banner" TO "test_color"`);
-        await queryRunner.query(`ALTER TYPE "public"."stats_text_alignment2_enum" RENAME TO "stats_text_alignment_enum"`);
-        await queryRunner.query(`ALTER TABLE "stats" RENAME COLUMN "text_alignment2" TO "text_alignment"`);
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`ALTER TABLE "creators" DROP COLUMN "twitch_banner"`)
+	}
 }
